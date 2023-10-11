dc1042385e629c5f003a6484ac9c7e895de053ec
Everton Daniel de Lima Romualdo <evertondanieldelimaromualdo@gmail.com>
2023-10-11 13:28:09 -0300

    Agora todas a funções pura, menu no arquivo main, opção sair adicionada

--- main.py
+++ main.py
@@ -6,32 +6,67 @@
 #Data de inicio 23/09/2023
 import funtions as fc
 
-while True:
-    fc.menu_Inicial()
-    opcao = input('O que deseja? ')
-    if opcao == "c":
-        fc.cadastrar()
-    elif opcao == 'l':
-        aux = fc.logar()
-        if aux == True:
-            opcao = ''
-            break
-    elif opcao == "adm":
-        fc.adm()
-    
+opcao_escolhida = '' #Variavel responsavel por coleta o pront do usuario para direcionalo para a função correta
+retorno_de_funcao = None #sempre vai receber apenas o retorno de alguma função.
+                         #O retorno é True para uma ação feita com sucesso, False caso contrario.
+usuario_logado = False #Indica se a um usurio online(True) ou não(False)
 
 while True:
-    fc.menu_principal()
-    opcao = input('O que deseja? ')
-    if opcao == "c":
-        fc.cadastrar()
-    elif opcao == 'l':
-        fc.logar()
-    elif opcao == "d":
-        fc.depositar()
-    elif opcao == "s":
-        fc.sacar()
-    elif opcao == "vs":
-        fc.ver_saldo()
-    elif opcao == "adm":
-        fc.adm()
+    if usuario_logado == False:
+        while True: # while principal do programa
+            #Menu inicial
+            print("Bem vindo")
+            print("Cadastrar Usuario [c]")
+            print("Fazer Login [l]")
+            opcao_escolhida = input("Selecione uma opção: ")
+            
+            if opcao_escolhida == "c":
+                nome = input("Digite seu nome: ")
+                senha = input("Digite sua senha")
+                retorno_de_funcao = fc.cadastrar(nome, senha)
+                if retorno_de_funcao == True:
+                    print("CADASTRO REALIZADO COM SUCESSO!")
+                else:
+                    print("ERRO: USUARIO JÁ EXISTENTE!")
+            elif opcao_escolhida == 'l':
+                nome = input("Digite seu nome de usuario: ")
+                senha = input("Digite sua senha: ")
+                retorno_de_funcao = fc.logar(nome, senha)
+                if retorno_de_funcao == True:
+                    print("LOGIN REALIZADO COM SUCESSO!")
+                    usuario_logado = True
+                    break
+                else:
+                    print("NOME DE USUARIO OU SENHA INVALIDOS!")
+    if usuario_logado == True:
+        while True:
+            print("Py Bank")
+            print()
+            print("Depositar [d]")
+            print("Sacar [s]")
+            print("Ver Saldo [vs]")
+            print("Sair [ex]")
+            opcao_escolhida = input("Selecione uma opção: ")
+
+            if opcao_escolhida == 'd':
+                valor = input("Digite o valor de deposito: ")
+                fc.depositar(valor)
+                print(f"DEPOSITO REALIZADO COM SUCESSO! NOVO SALDO DE R$ {fc.user_online[2]}")
+            elif opcao_escolhida == 's':
+                valor = input("Digite o valor do saque: ")
+                retorno_de_funcao = fc.sacar(valor)
+                if retorno_de_funcao == True:
+                    print(f"SAQUE REALIZADO COM SUCESSO! NOVO SALDO DE R${fc.user_online[2]}")
+                else:
+                    print(f"SALDO INSUFICIENTE! SEU SALDO É DE R${fc.user_online[2]}")
+            elif opcao_escolhida == 'vs':
+                print(f"SEU SALDO É DE R${fc.user_online[2]}")
+            elif opcao_escolhida == 'ex':
+                fc.sair()
+                usuario_logado = False
+                print("LOGOUT REALIZADO COM SUCESSO! ATÉ LOGO!")
+                break
+            
+            
+            
+
